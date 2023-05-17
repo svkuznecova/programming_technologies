0e9b4a0a079e9888f3bc7ffce5ef677f86e41fa2
svkuznecova <115889802+svkuznecova@users.noreply.github.com>
2023-05-17 23:24:50 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,99 +1,100 @@
-#include <iostream> 
-#include <vector> 
- 
-using namespace std; 
- 
-class Matrix { 
-private: 
-    int a; // строки 
-    int b; // столбцы 
-    vector<vector<double>>mat; 
- 
-public: 
-    Matrix(int numA, int numB) : a(numA), b(numB) { 
-        mat.resize(a, vector<double>(b)); 
-    } 
-// Ввод матрицы 
-    void input() { 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                cin >> mat[i][j]; 
-            } 
-        } 
-    } 
-// Вывод матрицы 
-    void print() { 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                cout << mat[i][j] << " "; 
-            } 
-            cout << endl; 
-        } 
-    } 
-// Оператор сложение матриц 
-    Matrix operator+(Matrix& other) { 
-        Matrix result(a, b); 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                result.mat[i][j] = mat[i][j] + other.mat[i][j]; 
-            } 
-        } 
-        return result; 
-    } 
-// Оператор вычитания матриц 
-    Matrix operator-(Matrix& other) { 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                result.mat[i][j] = mat[i][j] - other.mat[i][j]; 
-            } 
-        } 
-        return result; 
-    } 
-// Оператор умножения матриц 
-    Matrix operator*(Matrix& other) { 
-        int otherb = other.b; 
-        Matrix result(a, otherb); 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < otherb; j++) { 
-                for (int k = 0; k < b; k++) { 
-                    result.mat[i][j] += mat[i][k] * other.mat[k][j]; 
-                } 
-            } 
-        } 
-        return result; 
-    } 
-// Оператор умножения матриц на скаляр  
-    Matrix operator*(double scal) { 
-        Matrix result(a, b); 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                result.mat[i][j] = mat[i][j] * scal; 
-            } 
-        } 
-        return result; 
-    } 
-// Оператор деления матриц на скаляр 
-    Matrix operator/(double scal) { 
-        Matrix result(a, b); 
-        for (int i = 0; i < a; i++) { 
-            for (int j = 0; j < b; j++) { 
-                result.mat[i][j] = mat[i][j] / scal; 
-            } 
-        } 
-        return result; 
-    } 
-}; 
- 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Matrix {
+private:
+    int rows;
+    int cols;
+    vector<vector<double>> mat;
+
+public:
+    Matrix(int numRows, int numCols) : rows(numRows), cols(numCols) {
+        mat.resize(rows, vector<double>(cols));
+    }
+
+    void input() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cin >> mat[i][j];
+            }
+        }
+    }
+    
+    void print() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    Matrix operator+( Matrix& other) {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.mat[i][j] = mat[i][j] + other.mat[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator-(Matrix& other) {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.mat[i][j] = mat[i][j] - other.mat[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator*(Matrix& other) {
+        int otherCols = other.cols;
+        Matrix result(rows, otherCols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < otherCols; j++) {
+                for (int k = 0; k < cols; k++) {
+                    result.mat[i][j] += mat[i][k] * other.mat[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    Matrix operator*(double scalar) {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.mat[i][j] = mat[i][j] * scalar;
+            }
+        }
+        return result;
+    }
+
+    Matrix operator/(double scalar) {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.mat[i][j] = mat[i][j] / scalar;
+            }
+        }
+        return result;
+    }
+};
+
 int main() { 
-    int a, b; 
-    double scal; 
+    int rows, cols; 
+    double scalar; 
  
     cout << "Введите количество строк: "; 
-    cin >> a; 
+    cin >> rows; 
     cout << "Введите количество столбцов: "; 
-    cin >> b; 
+    cin >> cols; 
  
-    Matrix mat1(a, b), mat2(a, b); 
+    Matrix mat1(rows, cols), mat2(rows, cols); 
  
     cout << "Введите значения для первой матрицы:\n"; 
     mat1.input(); 
@@ -101,15 +102,15 @@ int main() {
     mat2.input(); 
  
     cout << "Введите скаляр:\n"; 
-    cin >> scal; 
+    cin >> scalar; 
  
     Matrix sum = mat1 + mat2; // Сложение матриц 
     Matrix raz = mat1 - mat2; // Вычитание матриц 
     Matrix ymn = mat1 * mat2; // Умножение матриц 
-    Matrix ymnskal1 = mat1 * scal; // Умножение 1-ой матрицы на скаляр  
-    Matrix delskal2 = mat2 / scal; // Деление 2-ой матрицы на скаляр  
-    Matrix ymnskal2 = mat2 * scal; // Умножение 2-ой матрицы на скаляр  
-    Matrix delskal1 = mat1 / scal; // Деление 1-ой матрицы на скаляр  
+    Matrix ymnskal1 = mat1 * scalar; // Умножение 1-ой матрицы на скаляр  
+    Matrix delskal2 = mat2 / scalar; // Деление 2-ой матрицы на скаляр  
+    Matrix ymnskal2 = mat2 * scalar; // Умножение 2-ой матрицы на скаляр  
+    Matrix delskal1 = mat1 / scalar; // Деление 1-ой матрицы на скаляр  
  
 //вывоод 
     cout << "Сумма матриц: " << endl; 
@@ -121,14 +122,14 @@ int main() {
     cout << "Умножегние матриц: " << endl; 
     ymn.print(); 
  
-    cout << "Умножение 1-ой матрицы на скаляр " << scal << ": " << endl; 
+    cout << "Умножение 1-ой матрицы на скаляр " << scalar << ": " << endl; 
     ymnskal1.print(); 
-    cout << "Деление 1-ой матрицы на скаляр  " << scal << ": " << endl; 
+    cout << "Деление 1-ой матрицы на скаляр  " << scalar << ": " << endl; 
     delskal1.print(); 
  
-    cout << "Умножение 2-ой матрица на скаляр  " << scal << ": " << endl; 
+    cout << "Умножение 2-ой матрица на скаляр  " << scalar << ": " << endl; 
     ymnskal2.print(); 
-    cout << "Деление 2-ой матрицы на скаляр  " << scal << ": " << endl; 
+    cout << "Деление 2-ой матрицы на скаляр  " << scalar << ": " << endl; 
     delskal2.print(); 
  
     return 0; 
