ab3e7cb22502f56c0757ced4492e5c76741afc66
svkuznecova <115889802+svkuznecova@users.noreply.github.com>
2023-10-09 21:14:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,143 +1,89 @@
-#include <iostream>
-#include <string>
-#include <memory>
+%газета(id, Название, Жанр, Цена).
+газета(1, "Некий кот", развлекательный, 150).
+газета(2, "Смешарики", детский, 120).
+газета(3, "Вокруг света", научный, 300).
+газета(4, "99 дней", развлекательный, 200).
+газета(5, "Дача", бытовой, 200).
+газета(6, "Рецепты", бытовой, 100).
+газета(7, "В мире науки", научный, 175).
+газета(7, "Вестник", новостной, 99).
+газета(8, "Мурзилка", детский, 199).
+газета(9, "Известия", новостной, 149).
+газета(10, "Баскетбол", спортивный, 100).
+газета(11, "Спорт сегодня", спортивный, 89).
+газета(12, "Молодая гвардия", политический, 100).
 
-// Описание структуры Node с полями типа string, int, double
-struct Node {
-std::string name;
-int integer;
-double floating_point;
+%подписчик(id, ФИО, возраст, адрес).
+подписчик(1, "Петухов Мирон Иванович", 7, "Озерная 55").
+подписчик(2, "Алексеев Леонид Егорович", 19, "Миклухо-Маклая 7к2").
+подписчик(3, "Овсянников Ярослав Ильич", 20, "Чкалова 45").
+подписчик(4, "Никитин Денис Ильич", 19, "Цветочная 23").
+подписчик(5, "Селиванов Леон Максимович", 27, "Петровская 3").
+подписчик(6, "Тихомирова Полина Кирилловна", 67, "Вокзальная 1").
+подписчик(7, "Маркин Артём Германович", 16, "Киролова 27").
+подписчик(8, "Ермаков Демид Никитич", 17, "Ульяновская 53").
+подписчик(9, "Селезнев Артём Дмитриевич", 25, "Берёзовская 27").
+подписчик(10, "Кудряшова Екатерина Алексеевна", 78, "Камнева 27").
+подписчик(11, "Сазанов Даниил Саввич", 20, "Солнечная 32").
 
-std::shared_ptr<Node> next;
+%факт: подписан(id_подписчика, id_подписки, длительность подписки в месяцах).
+подписан(1, 8, 3).
+подписан(1, 7, 2).
+подписан(2, 6, 4).
+подписан(2, 5, 1).
+подписан(3, 4, 7).
+подписан(3, 3, 1).
+подписан(4, 2, 2).
+подписан(4, 1, 4).
+подписан(5, 12, 6).
+подписан(5, 10, 2).
+подписан(6, 11, 7).
+подписан(6, 9, 1).
+подписан(7, 8, 9).
+подписан(7, 6, 1).
+подписан(8, 7, 8).
+подписан(8, 5, 10).
+подписан(9, 4, 2).
+подписан(9, 2, 4).
+подписан(10, 3, 5).
+подписан(10, 1, 12).
 
-Node(const std::string& name, int integer, double floating_point) :
-name(name), integer(integer), floating_point(floating_point), next(nullptr) {}
-};
+%правило: вывод название газеты и его цена по жанру.
+издание_газеты(Жанр, Название, Цена) :-
+газета(_, Название, Жанр, Цена).
 
-// Описание класса LinkedList для работы со списком на базе структуры Node
-class LinkedList {
-public:
-// Конструктор класса, инициализирует пустой список
-LinkedList() : head(nullptr), tail(nullptr) {}
+%правило: вывод названия газеты и суммы подписки по имени подписчика.
+сумма_подписки(Имя, Название, Сумма) :-
+газета(Id_газеты, Название, _, Цена),
+подписчик(Id_подписчика, Имя, _, _),
+подписан(Id_подписчика, Id_газеты, Длительность),
+Сумма is Длительность * Цена.
 
-// Функция добавления элемента в начало списка
-void add_to_beginning(const std::string& name, int integer, double floating_point) {
-std::shared_ptr<Node> new_node = std::make_shared<Node>(name, integer, floating_point);
-new_node->next = head;
-head = new_node;
-if (tail == nullptr) {
-tail = head;
-}
-}
+%правило: вывод названии газеты по имени подписчика.
+подписчики_газеты(Название, Имя) :-
+газета(Id_газеты, Название, _, _),
+подписчик(Id_подписчика, Имя, _, _),
+подписан(Id_подписчика, Id_газеты, _).
 
-// Функция добавления элемента в конец списка
-void add_to_end(const std::string& name, int integer, double floating_point) {
-std::shared_ptr<Node> new_node = std::make_shared<Node>(name, integer, floating_point);
-if (tail != nullptr) {
-tail->next = new_node;
-}
-tail = new_node;
-if (head == nullptr) {
-head = tail;
-}
-}
+%правило: вывод имени подписчика и название газеты по жанру.
+жанр(Жанр, Имя, Название):-
+газета(Id_газеты, Название, Жанр, _),
+подписан(Id_подписчика, Id_газеты, _),
+подписчик(Id_подписчика, Имя, _, _).
 
-// Функция добавления элемента после заданного элемента с именем target_name
-void add_after(const std::string& target_name, const std::string& name, int integer, double floating_point) {
-std::shared_ptr<Node> current = head;
-while (current != nullptr && current->name != target_name) {
-current = current->next;
-}
-if (current == nullptr) {
-return;
-}
-std::shared_ptr<Node> new_node = std::make_shared<Node>(name, integer, floating_point);
-new_node->next = current->next;
-current->next = new_node;
-if (tail == current) {
-tail = new_node;
-}
-}
+%правило: вывод улицы по названию газеты.
+доставка(Название, Адрес) :-
+газета(Id_газеты, Название, _, _),
+подписан(Id_подписчика, Id_газеты, _),
+подписчик(Id_подписчика, _, _, Адрес).
 
-// Функция добавления элемента перед заданным элементом с именем target_name
-void add_before(const std::string& target_name, const std::string& name, int integer, double floating_point) {
-if (head == nullptr || head->name == target_name) {
-add_to_beginning(name, integer, floating_point);
-return;
-}
-std::shared_ptr<Node> current = head;
-while (current->next != nullptr && current->next->name != target_name) {
-current = current->next;
-}
-if (current->next == nullptr) {
-return;
-}
-std::shared_ptr<Node> new_node = std::make_shared<Node>(name, integer, floating_point);
-new_node->next = current->next;
-current->next = new_node;
-}
+%правило; вывод возраста подписчика по его имени.
+возраст_пописчика(Имя, Возраст) :-
+подписчик(_, Имя, Возраст, _).
 
-// Функция удаления элемента с заданным именем name
-void remove(const std::string& name) {
-if (head == nullptr) {
-return;
-}
-if (head->name == name) {
-head = head->next;
-if (head == nullptr) {
-tail = nullptr;
-}
-return;
-}
-std::shared_ptr<Node> current = head;
-while (current->next != nullptr && current->next->name != name) {
-current = current->next;
-}
-if (current->next == nullptr) {
-return;
-}
-current->next = current->next->next;
-if (current->next == nullptr) {
-tail = current;
-}
-}
-
-// Функция вывода содержания списка на экран
-void display() const {
-std::shared_ptr<Node> current = head;
-while (current != nullptr) {
-std::cout << "Name: " << current->name << ", Integer: " << current->integer << ", Double: " << current->floating_point << std::endl;
-current = current->next;
-}
-}
-
-private:
-std::shared_ptr<Node> head;
-std::shared_ptr<Node> tail;
-};
-// Функция, содержащая сценарий работы со списком с использованием разработанного инструментария
-void test_scenario() {
-LinkedList list;
-
-// Добавление элементов в начало и конец списка
-list.add_to_beginning("A", 1, 1.1);
-list.add_to_end("C", 3, 3.3);
-list.add_to_beginning("B", 2, 2.2);
-
-// Добавление элементов после и перед заданным элементом
-list.add_after("A", "D", 4, 4.4);
-list.add_before("D", "E", 5, 5.5);
-
-// Вывод содержания списка на экран
-list.display();
-
-// Удаление элемента с заданным именем
-list.remove("D");
-
-// Вывод содержания списка на экран
-list.display();
-}
-int main() {
-test_scenario();
-return 0;
-}
+%издание_газеты("спортивный", Название, Цена).
+%сумма_подписки("Селезнев Артём Дмитриевич", Название, Сумма).
+%подписчики_газеты(Название, "Селиванов Леон Максимович").
+%жанр("детский", Имя, Название).
+%доставка("Дача", Адрес).
+%возраст_пописчика("Кудряшова Екатерина Алексеевна", Возраст).
